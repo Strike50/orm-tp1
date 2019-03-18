e7ea2a54fb06906a3b47495cc888a64d19d32d32
Laplijr <ronanlaplaud4@gmail.com>
2019-03-18 14:33:06 +0100

    Matiere

---
+++ src/main/java/fr/epsi/model/Matiere.java
@@ -0,0 +1,29 @@
+package fr.epsi.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class Matiere implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+
+    public Matiere() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+}

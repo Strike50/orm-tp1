c051748df01796967447c26081cf83705b891aa1
Houlgatte Louka <lhoulgattebustamante@gmail.com>
2019-03-20 17:22:47 +0100

    try some fix hirebante cfg

--- src/main/java/fr/epsi/utils/HibernateUtil.java
+++ src/main/java/fr/epsi/utils/HibernateUtil.java
@@ -10,10 +10,11 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            Configuration cfg = new Configuration();
-            cfg.configure("main/resources/hibernate.cfg.xml");
-            cfg.setProperty("hibernate.connection.password", System.getenv("DBMDP"));
-            return cfg.buildSessionFactory();
+//            Configuration cfg = new Configuration();
+//            cfg.configure("hibernate.cfg.xml");
+//            cfg.setProperty("hibernate.connection.password", System.getenv("DBMDP"));
+//            return cfg.buildSessionFactory();
+            return new Configuration().configure().buildSessionFactory();
         }
         catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
